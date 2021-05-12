16253d1fdf4b5c0d05363d8d49e168b3705121b3
Kascique <kascique.lowmans@gmail.com>
2021-05-11 23:11:47 -0400

    smart_tools:

--- smart_tools.py
+++ smart_tools.py
@@ -1,1 +1,16 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+
+import uuid
+
+from datetime import date
+from datetime import datetime
+
+today = date.today()
+now = datetime.now()
+
+portal = Blueprint('portal', __name__)
+
+date_format = "%m/%d/%Y"
+time_format = "%H:%M:%S"
+date_time_format = "%m/%d/%Y %H:%M:%S"
+
