13e9d3f3e22650ca661ac40c6d24a086e2fd6723
Kascique <kascique.lowmans@gmail.com>
2021-05-13 06:03:10 -0400

    udpate

--- smart_tools.py
+++ smart_tools.py
@@ -27,11 +27,11 @@ def encrypt():
     password = request.form.get('password')
     file = request.files['file']
 
-    print(password)
-    print(file)
+    # print(file)
 
-    filename = secure_filename(file.filename)
-    file.save(os.path.join('/Uploads/', filename))
+    # filename = secure_filename(file.filename)
+    # file.save(os.path.join('/Uploads/', filename))
+
     # return redirect(url_for('uploaded_file',
     #                         filename=filename))
 
