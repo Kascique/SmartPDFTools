8ba6e7a634491ca82d95d4ff539fdc3ab9297a75
Kascique <kascique.lowmans@gmail.com>
2021-05-13 14:24:08 -0400

    update

--- smart_tools.py
+++ smart_tools.py
@@ -34,13 +34,10 @@ def encrypt():
 
     with open(filename, "rb") as in_file:
         input_pdf = PdfFileReader(in_file)
-
-    output_pdf = PdfFileWriter()
-    output_pdf.appendPagesFromReader(input_pdf)
-    output_pdf.encrypt("password")
-
-    # with open(encrypt_path, "wb") as out_file:
-    #     output_pdf.write(out_file)
-
+        output_pdf = PdfFileWriter()
+        output_pdf.appendPagesFromReader(input_pdf)
+        output_pdf.encrypt("password")
+        with open(encrypt_path, "wb") as out_file:
+            output_pdf.write(out_file)
 
     return redirect(url_for('smart_tools.index'))
